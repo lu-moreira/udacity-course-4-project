ce374e215eaa5f477bbf9c2d3bf26b7830318822
Lucas Moreira <lucas.moreira@mercadolivre.com>
2020-02-21 19:09:16 -0300

    Move s3 actions to utils/s3.ts file

--- backend/src/lambda/http/generateUploadUrl.ts
+++ backend/src/lambda/http/generateUploadUrl.ts
@@ -1,15 +1,10 @@
 import 'source-map-support/register'
-import * as AWS from 'aws-sdk';
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import { recoverS3PreSignedUrl } from '../../utils/s3';
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const todoId = event.pathParameters.todoId
-  const s3 = new AWS.S3({ signatureVersion: 'v4' });
-  const uploadUrl = s3.getSignedUrl('putObject', {
-    Bucket: process.env.FILE_UPLOAD_S3_BUCKET,
-    Key: todoId,
-    Expires: 600 // Expires in 600s
-  })
+  const uploadUrl = recoverS3PreSignedUrl(todoId)
   return {
     statusCode: 200,
     headers: {
